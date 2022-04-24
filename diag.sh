5d0cfa4f9541644bf9be1b62857ae890e27acd78
cheki <borna@986@gmail.com>
2022-04-24 11:39:36 +0200

    update

--- diag.sh
+++ diag.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 IN=$(kubectl get pods -o=name --field-selector=status.phase=Running | grep p2pnode)
 readarray -t fields <<<"$IN"
+separator='------------------------------------'
 for node in "${fields[@]}" ;do
-    printf '%s\n' "--- listing lsitening ports on node: ${node}"
-    kubectl exec -it "$node" -- netstat -tlpn | grep 172.17
+    printf '%s' "--- listing diagnostics of node: ${node}"
+    printf '\n\n'
+    kubectl describe "${node}"
+    printf '\n'
+    printf '%s\n' "${separator}" 
 done
-echo "done!"
+echo "--- listing pv"
+kubectl get pv -o wide
+echo "--- listing pvc"
+kubectl get pvc -o wide
+echo "--- listing running pods"
+kubectl get pods -o wide --field-selector=status.phase=Running
 exit 0
